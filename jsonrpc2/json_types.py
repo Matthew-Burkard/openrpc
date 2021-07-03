7c2d29190cd3433c93b18064c89a2aa00301dcec
Matthew Burkard <matthewjburkard@gmail.com>
2021-07-03 09:34:10 -0400

    refactor: make collection type hints consistent
    Use "list" and "dict" in json_types rather than typing "List" or "Dict" to be consistent with the rest of the project.

--- jsonrpc2/json_types.py
+++ jsonrpc2/json_types.py
@@ -1,7 +1,7 @@
 from numbers import Number
-from typing import Union, Optional, List, Dict
+from typing import Union, Optional
 
 JSONPrimitive = Optional[Union[str, Number, bool]]
-JSONArray = List[Union[JSONPrimitive, 'JSONObject', 'JSONArray']]
-JSONObject = Dict[str, Union[JSONPrimitive, 'JSONObject', 'JSONArray']]
+JSONArray = list[Union[JSONPrimitive, 'JSONObject', 'JSONArray']]
+JSONObject = dict[str, Union[JSONPrimitive, 'JSONObject', 'JSONArray']]
 JSON = Union[JSONPrimitive, JSONArray, JSONObject]
