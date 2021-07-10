21784d13805e5c43f35d81fff375709ce8380eb4
Matthew Burkard <matthewjburkard@gmail.com>
2021-07-10 15:11:03 -0400

    feat: add direct client
    Add an implementation of RPCClient called RPCDirectClient which takes an RPCServer and calls its methods directly.

--- demo/client.py
+++ demo/client.py
@@ -1,28 +1,21 @@
 import uuid
-from typing import Union, Any
+from typing import Union
 
-from jsonrpc2.rpc_client import RPCClient
+from jsonrpc2.rpc_client import RPCDirectClient
 from jsonrpc2.rpc_objects import RPCError, RPCRequest
-from jsonrpc2.rpc_server import RPCServer
 
 
 def get_uuid() -> str:
     return str(uuid.uuid4())
 
 
-class MathRPCClient(RPCClient):
-
-    def __init__(self, server: RPCServer):
-        self.server = server
+class MathRPCClient(RPCDirectClient):
 
     def add(self, x: float, y: float) -> Union[float, RPCError]:
-        return self._call(RPCRequest('add', [x, y], str(uuid.uuid4())))
+        return self._call(RPCRequest('add', [x, y], str(uuid.uuid4())), )
 
     def subtract(self, x: float, y: float) -> Union[float, RPCError]:
-        return self._call(RPCRequest('subtract', [x, y], str(uuid.uuid4())))
+        return self._call(RPCRequest('subtract', [x, y], str(uuid.uuid4())), )
 
     def divide(self, x: float, y: float) -> Union[float, RPCError]:
-        return self._call(RPCRequest('divide', [x, y], str(uuid.uuid4())))
-
-    def _call(self, request: RPCRequest) -> Any:
-        return self._handle_json(self.server.process(request.to_json()))
+        return self._call(RPCRequest('divide', [x, y], str(uuid.uuid4())), )
--- jsonrpc2/rpc_client.py
+++ jsonrpc2/rpc_client.py
@@ -7,7 +7,10 @@ from typing import Any, Union, Type, Optional, Callable
 from jsonrpc2.exceptions import get_exception, JSONRPCError, ServerError
 from jsonrpc2.rpc_objects import RPCRequest, RPCResponse
 
-__all__ = ('RPCClient',)
+__all__ = ('RPCClient', 'RPCDirectClient')
+
+from jsonrpc2.rpc_server import RPCServer
+
 log = logging.getLogger(__name__)
 
 
@@ -40,3 +43,15 @@ class RPCClient(abc.ABC):
         except (JSONDecodeError, TypeError, AttributeError) as e:
             log.exception(f'{type(e).__name__}:')
             raise JSONRPCError('Unable to parse response.')
+
+
+class RPCDirectClient(RPCClient):
+    def __init__(self, server: RPCServer):
+        self.server = server
+
+    def _call(
+            self,
+            request: RPCRequest,
+            deserializer: Optional[Callable] = None
+    ) -> Any:
+        return self._handle_json(self.server.process(request.to_json()))
