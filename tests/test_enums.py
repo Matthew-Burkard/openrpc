75e488ecbebf5d15668307333d807b98b2852ab0
Matthew Burkard <matthewjburkard@gmail.com>
2022-01-05 22:43:45 -0500

    refactor: make enum fields UPPER_SNAKE_CASE

--- tests/test_enums.py
+++ tests/test_enums.py
@@ -9,35 +9,36 @@ from pydantic import BaseModel
 class Model(BaseModel):
     """Example type for the enum."""
 
-    int_field: int
+    INT_FIELD: int
 
 
 class EnumExample(Enum):
     """Each type for options should get a JSON Schema type."""
 
-    int_option: int = 3
-    str_option: str = 'A string with a "'
+    INT_OPTION: int = 3
+    STR_OPTION: str = 'A string with a "'
 
 
 # TODO Determine if JSON Schema spec supports non-primitive enum values.
 class EnumClassFieldExample(Enum):
     """Enum with a field of a custom class type."""
 
-    class_option: Model
+    CLASS_OPTION: Model
 
 
 class EnumUnsetExample(Enum):
     """Enum with a field of a custom class type."""
 
-    no_value: float
+    NO_VALUE: float
 
 
 class EnumExampleWithNullable(Enum):
     """If any field is nullable "null" should be a valid type."""
 
-    str_option: str = r'\"\\"'
-    opt_int_option: Optional[int] = None
+    STR_OPTION: str = r'\"\\"'
+    OPT_INT_OPTION: Optional[int] = None
 
 
 class EnumTest(unittest.TestCase):
-    pass
+    def test_register_enum_using_method(self) -> None:
+        pass
