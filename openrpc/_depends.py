c062c05a6d22bb129b9dec0de20344bcb7351876
Matthew Burkard <matthewjburkard@gmail.com>
2023-03-07 21:54:35 -0500

    fix: `Depends` works with type checkers

--- openrpc/_depends.py
+++ openrpc/_depends.py
@@ -1,5 +1,9 @@
 """Module providing class to handle middleware dependencies."""
+from typing import Any
 
 
-class Depends:
+class _Depends:
     """Used to identify a param as a dependency."""
+
+
+Depends: Any = _Depends
