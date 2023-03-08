6542a582a642be7bc23c5e555cd86c6e9b04aa13
Matthew Burkard <matthewjburkard@gmail.com>
2023-03-07 21:50:20 -0500

    test: add unit test for param shadowing dependency names

--- tests/test_depends.py
+++ tests/test_depends.py
@@ -15,6 +15,12 @@ def method_with_dep(arg: int, dep: str = Depends) -> str:
 
 
 @rpc.method
+def method_without_dep(dep: int) -> int:
+    """Method without dep and param shadowing other method dep name."""
+    return dep
+
+
+@rpc.method
 async def async_method_with_dep(arg: int, dep: str = Depends) -> str:
     """Method with dependency to test."""
     return f"{arg}-{dep}"
@@ -54,6 +60,17 @@ def test_depends_extra_dependencies() -> None:
         rpc.process_request(json.dumps(req), {"dep": user, "name": name})
     )
     assert result["result"] == f"1-{user}"
+
+
+def test_depends_shadow_dep_name() -> None:
+    req = {
+        "id": 1,
+        "method": "method_without_dep",
+        "params": {"dep": 1},
+        "jsonrpc": "2.0",
+    }
+    result = json.loads(rpc.process_request(json.dumps(req), {"dep": "Mocha"}))
+    assert result["result"] == 1
 
 
 @pytest.mark.asyncio
