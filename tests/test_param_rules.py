c66fa5871c8c77f139f53a935a10f9e7d424461b
Matthew Burkard <matthewjburkard@gmail.com>
2023-04-08 13:56:07 -0400

    fix: enforce openrpc param structure

---
+++ tests/test_param_rules.py
@@ -0,0 +1,58 @@
+"""Test param rules."""
+import json
+
+from openrpc import ParamStructure, RPCServer
+
+rpc = RPCServer(title="Test Depends", version="0.1.0")
+rpc_catch_all = RPCServer(title="Test Depends", version="0.1.0")
+error_message = "Custom error message"
+
+
+@rpc.method(param_structure=ParamStructure.BY_POSITION)
+def position_add(a: int, b: int) -> int:
+    """Add with params by-position."""
+    return a + b
+
+
+@rpc.method(param_structure=ParamStructure.BY_NAME)
+def name_add(a: int, b: int) -> int:
+    """Add with params by-position."""
+    return a + b
+
+
+def test_by_position() -> None:
+    position_req = {
+        "id": 1,
+        "method": "position_add",
+        "params": [2, 2],
+        "jsonrpc": "2.0",
+    }
+    result = json.loads(rpc.process_request(json.dumps(position_req)))
+    assert result["result"] == 4
+    name_req = {
+        "id": 1,
+        "method": "position_add",
+        "params": {"a": 2, "b": 2},
+        "jsonrpc": "2.0",
+    }
+    result = json.loads(rpc.process_request(json.dumps(name_req)))
+    assert result["error"]["data"] == "Params must be passed by position."
+
+
+def test_by_name() -> None:
+    name_req = {
+        "id": 1,
+        "method": "name_add",
+        "params": {"a": 2, "b": 2},
+        "jsonrpc": "2.0",
+    }
+    result = json.loads(rpc.process_request(json.dumps(name_req)))
+    assert result["result"] == 4
+    position_req = {
+        "id": 1,
+        "method": "name_add",
+        "params": [2, 2],
+        "jsonrpc": "2.0",
+    }
+    result = json.loads(rpc.process_request(json.dumps(position_req)))
+    assert result["error"]["data"] == "Params must be passed by name."
