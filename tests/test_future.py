b29199fbe4ed98c1b39289b6c90e85f29ed9825a
Matthew Burkard <matthewjburkard@gmail.com>
2023-08-20 14:10:40 -0400

    test: fix structure of future annotations test

--- tests/test_future.py
+++ tests/test_future.py
@@ -7,21 +7,36 @@ rpc = RPCServer(title="Test OpenRPC", version="1.0.0", debug=True)
 
 
 @rpc.method()
-def test_future(_list_str: list[str] | None = None) -> None:
-    assert rpc.discover()["methods"][0] == {
-        "examples": [{"params": [], "result": {"value": None}}],
-        "name": "test_future",
-        "params": [
-            {
-                "name": "_list_str",
-                "required": False,
-                "schema": {
-                    "anyOf": [
-                        {"items": {"type": "string"}, "type": "array"},
-                        {"type": "null"},
-                    ]
-                },
-            }
-        ],
-        "result": {"name": "result", "required": True, "schema": {"type": "null"}},
-    }
+def future(list_str: list[str] | None = None) -> list[str]:
+    """Function using future union syntax."""
+    return list_str or []
+
+
+def test_future() -> None:
+    method = rpc.discover()["methods"][0]
+    # Examples
+    assert [
+        {
+            "params": [{"name": "list_str", "value": ["string"]}],
+            "result": {"value": ["string"]},
+        }
+    ] == method["examples"]
+    # Params
+    assert [
+        {
+            "name": "list_str",
+            "required": False,
+            "schema": {
+                "anyOf": [
+                    {"items": {"type": "string"}, "type": "array"},
+                    {"type": "null"},
+                ]
+            },
+        }
+    ] == method["params"]
+    # Result
+    assert {
+        "name": "result",
+        "required": True,
+        "schema": {"items": {"type": "string"}, "type": "array"},
+    } == method["result"]
